081c8b937250f594b344e0392559d63046b63a1a
Josue Abraham <josuea@noukod.com>
2019-02-01 17:42:09 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-
-  resources :authors, only: [:show, :index] do
-    resources :posts, only: [:show, :index, :new, :edit]
+  scope '/admin' do
+    resources :stats, only: [:index]
   end
-
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
-
-  root 'posts#index'
-end
